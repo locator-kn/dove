0fa9ee7d705a3aec11807da9d83e1e1f137f545e
Timo Weiß <info@timo-weiss.com>
2015-06-14 20:36:55 +0200

    simple impl

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var server = new Hapi.Server();
 server.connection({port: 3030});
 
 
+server.route({
+    method: 'POST',
+    path: '/register',
+    handler: function (request, reply) {
+        reply('success');
+    }
+});
+
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
