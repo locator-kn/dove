40bb87b08a60fa47fe57073254db4f6dfada75ef
Timo Weiß <info@timo-weiss.com>
2015-06-14 20:45:26 +0200

    add validation

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var Hapi = require('hapi');
+var joi = require('joi');
 
 var server = new Hapi.Server();
 server.connection({port: 3030});
+
+var schema = joi.object().keys({
+    name: joi.string().alphanum().min(3).max(30).required(),
+    mail: joi.string().email().required()
+});
 
 
 server.route({
     method: 'POST',
     path: '/register',
-    handler: function (request, reply) {
-        reply('success');
+    config: {
+        handler: function (request, reply) {
+            reply('success');
+        },
+        validate: {
+            payload: schema
+        }
     }
 });
 
