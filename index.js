31c63a2f1efc91219d8255ca599e9525d42b1ad8
Timo Weiß <info@timo-weiss.com>
2015-06-15 19:31:53 +0200

    add feedback route

--- index.js
+++ index.js
@@ -7,21 +7,41 @@ var utils = require('./lib/utils');
 var server = new Hapi.Server();
 server.connection({port: 3030});
 
-var schema = joi.object().keys({
+var schemaUser = joi.object().keys({
     name: joi.string().alphanum().min(3).max(30).required(),
     mail: joi.string().email().required()
+});
+
+var schemaFeedback = joi.object().keys({
+    name: joi.string().alphanum().min(3).max(30).required(),
+    mail: joi.string().email().required(),
+    subject: joi.string().required(),
+    message: joi.string().required()
 });
 
 
 server.route({
     method: 'POST',
-    path: '/register',
+    path: '/mail/register',
     config: {
         handler: function (request, reply) {
             reply(utils.addUser(request.payload));
         },
         validate: {
-            payload: schema
+            payload: schemaUser
+        }
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/mail/feedback',
+    config: {
+        handler: function (request, reply) {
+            reply(utils.addFeedback(request.payload));
+        },
+        validate: {
+            payload: schemaFeedback
         }
     }
 });
